714b9b25fd6a6a5b3bb8cf878613ad80f648562b
Hugo Heagren <hugo@heagren.com>
2023-01-17 22:13:57 +0000

    Support places other than files for jumping to/from
    Fix #5.
    
    * related-files.el (related-files-current-place-finders): list of ways
    to get the current place as different types of value
    (related-files-jump,related-files-make,related-files-jump-or-make,
    related-files--choose-place): Update docs and variable names.
    (related-files-apply): make default method return nil. Move code for
    function jumpers to a specialised method.
    (related-files-get-filler): remove default method. Move code for
    function fillers to a specialised method.
    (related-files-place-exists-p): new generic. Replaces use of
    `file-exists-p' in previous code which assumed all places were
    filepaths.
    (related-files-goto-place): new generic. Replaces use of `find-file'
    in previous code.
    (related-files-attach-jumper-to-place): new generic. Replaces use of
    `propertize' in previous code.
    (related-files-retrieve-jumper-from-place): new generic. Complement
    to `related-files-attach-jumper-to-place'.
    (related-files-format-place): new generic. Formats non-filepath places
    as strings, for display in completing-read or similar interfaces.
    (related-files--get-current-places): new function. Replaces use
    `buffer-file-name' in previous code.
    (related-files--get-place-category): new function. Get completion
    category for a place, so that completing-read interfaces can be
    integrated with tools like Embark and Marginalia.
    (related-files--make-place,related-files--jump-or-make): use new
    functions which do not assume places are filepaths.
    (related-files--call-jumpers,related-files--attach-jumper-to-places):
    cosmetic changes.
    (related-files--completing-read): Format all entities properly, add
    support for multiple completion categories (for Embark/Marginalia
    compat), update docs
    * related-files-file.el: Move/add code for using files as places.
    * related-files-buffer.el: Move/add code for using buffers as places.
    * related-files-recipe.el (related-files-apply): specialise PLACE
    argument as a string, since recipe jumpers only support filepaths.
    * tests/related-files-test.el: update tests for new API.

---
+++ related-files-buffer.el
@@ -0,0 +1,66 @@
+;;; related-files-buffer.el --- Add support for buffer-based places to related-files  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Hugo Heagren
+
+;; Author: Hugo Heagren <hugo@undertown>
+;; Package-Requires: ((emacs "28.2"))
+;; Version: 1.0
+;; Keywords: convenience
+;; URL: https://www.gnu.org/software/emacs/
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provide buffers as a type of place for `related-files.el'.
+
+;;; Code:
+
+(require 'cl-lib)
+
+
+
+;;;###autoload
+(cl-defmethod related-files-place-exists-p ((place buffer))
+  "Call `buffer-live-p' on PLACE."
+  (buffer-live-p place))
+
+;;;###autoload
+(cl-defmethod related-files-goto-place ((place buffer))
+  "Call `switch-to-buffer' on PLACE."
+  (switch-to-buffer place))
+
+;;;###autoload
+(cl-defmethod related-files-attach-jumper-to-place (jumper (place buffer))
+  "Set `related-files-jumper' to JUMPER, locally in PLACE."
+  (with-current-buffer place
+    (setq-local related-files-jumper jumper))
+  place)
+
+;;;###autoload
+(cl-defmethod related-files-retrieve-jumper-from-place ((place buffer))
+  "Get the value of `related-files-jumper' in PLACE."
+  (defvar related-files-jumper)
+  (with-current-buffer place
+    related-files-jumper))
+
+;;;###autoload
+(cl-defmethod related-files-format-place (_initial-places (place buffer) &optional _annotate)
+  "Call `buffer-name' on PLACE.
+
+Ignore INITIAL-PLACES and ANNOTATE."
+  (buffer-name place))
+
+(provide 'related-files-buffer)
+;;; related-files-buffer.el ends here
--- tests/related-files-test.el
+++ tests/related-files-test.el
@@ -27,6 +27,7 @@
 
 ;;; Code:
 (require 'related-files)
+(require 'related-files-file)
 (require 'ert)
 (require 'cl-lib)
 (require 'seq)
@@ -39,14 +40,36 @@
   (should-not (safe-local-variable-p 'related-files-jumpers (list (lambda (place) place)))))
 
 
+;;; Define test jumpers
+
+(defalias 'related-files-test-jumper-identity #'identity)
+
+(defun related-files-test-jumper-const (_place)
+  "Ignore arguments, return symbol place."
+  'place)
+
+(defun related-files-test-jumper-atom (_place)
+  "Return \"/foo\"."
+  "/foo")
+
+(defun related-files-test-jumper-list (_place)
+  "Return (\"/bar1\" \"/bar2\")."
+  '("/bar1" "/bar2"))
+
+(defun related-files-test-jumper-singleton (_place)
+  "Return (\"/baz\")."
+  '("/baz"))
+
+(defalias 'related-files-test-jumper-nil #'ignore)
+
+(defvar related-files-test-jumper-args nil)
+
+
 ;;; Jumpers Public API
 
 (ert-deftest related-files-test-apply-function-jumper ()
-  (let* ((place 'place)
-         (jumperIdentity #'identity)
-         (jumperConst (lambda (_) place)))
-    (should (equal (related-files-apply jumperIdentity "/foo/bar") "/foo/bar"))
-    (should (equal (related-files-apply jumperConst "/foo/bar") place))))
+  (should (equal (related-files-apply 'related-files-test-jumper-identity "/foo/bar") "/foo/bar"))
+  (should (equal (related-files-apply 'related-files-test-jumper-const "/foo/bar") 'place)))
 
 (defun related-files-test-jumper-with-filler (_)
   "A jumper returning a constant place."
@@ -63,9 +86,9 @@
 (ert-deftest related-files-test-format-place ()
   (cl-letf (((symbol-function 'file-exists-p)
              (apply-partially #'equal "/project/foo/exists.el")))
-    (should (equal (related-files--format-place "/project/foo/" "/project/foo/exists.el") "exists.el"))
-    (should (equal (related-files--format-place "/project/bar/" "/project/foo/exists.el") "../foo/exists.el"))
-    (should (equal (related-files--format-place "/project/foo/" "/project/foo/non-existing.el") "non-existing.el (create it!)"))))
+    (should (equal (related-files--format-place '("/project/foo/") "/project/foo/exists.el") "exists.el"))
+    (should (equal (related-files--format-place '("/project/bar/") "/project/foo/exists.el") "../foo/exists.el"))
+    (should (equal (related-files--format-place '("/project/foo/") "/project/foo/non-existing.el" 'annotate) "non-existing.el (create it!)"))))
 
 
 ;;; Utility Functions
@@ -73,71 +96,74 @@
 (ert-deftest related-files-test-collect-existing-places-does-not-return-current-place ()
   (cl-letf (((symbol-function 'file-exists-p)
              (apply-partially #'seq-contains-p '("/bar" "/foo"))))
-    (let* ((current-place "/bar")
-           (new-place "/foo")
-           (jumper1 (lambda (_) new-place)))
+    (let ((current-place "/bar"))
       (should (equal
-               (related-files--collect-existing-places (list jumper1) current-place)
-               (list new-place))))))
+               (related-files--collect-existing-places '(related-files-test-jumper-atom) `(,current-place))
+               '("/foo"))))))
 
 (ert-deftest related-files-test-collect-existing-places-returns-uniq-results ()
   "If 2 jumpers produce the same place, the place should only appear once."
   (cl-letf (((symbol-function 'file-exists-p)
              (apply-partially #'seq-contains-p '("/bar" "/foo"))))
-    (let* ((current-place "/bar")
-           (new-place "/foo")
-           (jumper1 (lambda (_) new-place))
-           (jumper2 (lambda (_) new-place)))
+    (let ((current-place "/bar"))
       (should (seq-set-equal-p
-               (related-files--collect-existing-places (list jumper1 jumper2) current-place)
-               (list new-place))))))
+               (related-files--collect-existing-places
+		'(related-files-test-jumper-atom related-files-test-jumper-atom)
+		`(,current-place))
+               '("/foo"))))))
 
-(ert-deftest related-files-test-collect-existing-places-avoids-calling-same-jumper-with-current-place-twice ()
-  "A jumper must only be called once per place."
-  (cl-letf (((symbol-function 'file-exists-p) #'identity))
-    (let* ((current-place "/bar")
-           (jumper1-arguments nil)
-           (jumper1 (lambda (place) (push place jumper1-arguments) (list current-place "/place"))))
-      (related-files--collect-existing-places (list jumper1) current-place)
-      (should (seq-set-equal-p
-               jumper1-arguments
-               (list current-place current-place "/place"))))))
+(let ((jumper1-arguments nil)
+      (current-place "/bar"))
+  (defun related-files-test-jumper (place)
+    (push place jumper1-arguments)
+    (list current-place "/place"))
+  (ert-deftest related-files-test-collect-existing-places-avoids-calling-same-jumper-with-current-place-twice ()
+    "A jumper must only be called once per place."
+    (cl-letf (((symbol-function 'file-exists-p) #'identity))
+      (let* ()
+        (related-files--collect-existing-places (list 'related-files-test-jumper) (list current-place))
+        (should (seq-set-equal-p
+                 jumper1-arguments
+                 (list "/place" current-place)))))))
 
 (ert-deftest related-files-test-collect-existing-places-returns-no-place-when-no-current-place ()
   "If there is no current place, there shouldn't be any destination place."
   (should-not (related-files--collect-existing-places '(jumper) nil)))
 
 (ert-deftest related-files-test-call-jumpers ()
-  (let* ((jumperAtom (lambda (_) "/foo"))
-         (jumperList (lambda (_) (list "/bar1" "/bar2")))
-         (jumperSingleton (lambda (_) (list "/baz")))
-         (jumperNil (lambda (_)))
-         (jumperIdentity #'identity))
-    (should (seq-set-equal-p (related-files--call-jumpers
-                              (list jumperAtom jumperList)
-                              "/")
-                             '("/foo" "/bar1" "/bar2")))
-    (should (seq-set-equal-p (related-files--call-jumpers
-                              (list jumperAtom jumperSingleton)
-                              "/")
-                             '("/foo" "/baz")))
-    (should (seq-set-equal-p (related-files--call-jumpers
-                              (list jumperAtom jumperNil)
-                              "/")
-                             '("/foo")))
-    (should (seq-set-equal-p (related-files--call-jumpers
-                              (list jumperAtom jumperIdentity)
-                              '"/")
-                             '("/foo" "/")))
-    (should (seq-set-equal-p (related-files--call-jumpers
-                              (list jumperAtom jumperList jumperSingleton jumperNil jumperIdentity)
-                              '"/")
-                             '("/foo" "/bar1" "/bar2" "/baz" "/")))))
+  (should (seq-set-equal-p (related-files--call-jumpers
+			    '(related-files-test-jumper-atom
+			      related-files-test-jumper-list)
+                            '("/"))
+                           '("/foo" "/bar1" "/bar2")))
+  (should (seq-set-equal-p (related-files--call-jumpers
+			    '(related-files-test-jumper-atom
+			      related-files-test-jumper-singleton)
+			    '("/"))
+			   '("/foo" "/baz")))
+  (should (seq-set-equal-p (related-files--call-jumpers
+			    '(related-files-test-jumper-atom
+			      related-files-test-jumper-nil)
+			    '("/"))
+			   '("/foo")))
+  (should (seq-set-equal-p (related-files--call-jumpers
+			    '(related-files-test-jumper-atom
+			      related-files-test-jumper-identity)
+			    '("/"))
+			   '("/foo" "/")))
+  (should (seq-set-equal-p (related-files--call-jumpers
+			    '(related-files-test-jumper-atom
+			      related-files-test-jumper-singleton
+			      related-files-test-jumper-nil
+			      related-files-test-jumper-identity
+			      related-files-test-jumper-list)
+			    '("/"))
+			   '("/foo" "/bar1" "/bar2" "/baz" "/"))))
 
 (ert-deftest related-files-test-test--call-jumpers-attach-jumper-to-all-places ()
-  (let* ((jumper (lambda (_) "/foo"))
+  (let* ((jumper 'related-files-test-jumper-atom)
          (place (car (related-files--call-jumpers (list jumper) "/"))))
-    (should (eq (get-text-property 0 :related-files-jumper place) jumper))))
+    (should (eq (related-files-retrieve-jumper-from-place place) jumper))))
 
 (provide 'related-files-test)
 ;;; related-files-test.el ends here
